c75723d99f1c9676db77bb5848463c7171259768
tea <tea@teapc.mail>
2021-01-18 17:15:19 -0500

    Remove deprecated and unused utility functions

--- Scripts/utils.js
+++ Scripts/utils.js
@@ -14,11 +14,6 @@ function isPlayer(ply)
     return ply != null && ply.constructor.name == "Player"
 }
 
-function isServerHost(ply)
-{
-    return isPlayer(ply) && ply === world.getAllPlayers()[0]
-}
-
 function isGameObject(obj)
 {
     return obj != null && ply.constructor.name == "GameObject"
@@ -33,54 +28,7 @@ function format(str, ...args) {
     });
 }
 
-// this can act really odd due to gravity
-/**
- * "Smootly" moves an object in a straight line to the given position.
- *
- * @param {GameObject} obj - The object to mpve
- * @param {Vector} pos - Where the object is moving to
- * @param {function} callback - The function to call when the object has finished moving
- * @param {number} [speed=10] - Interpolation speed
- */
-function setPositionSmooth(obj, pos, callback, speed=10)
-{
-    if (!isValidGameObject(obj)) throw "Invalid GameObject"
-
-    let oldType = obj.getObjectType()
-    let moving = true
-
-    function smoothOnTick(self, dt)
-    {
-        let newPos = Vector.interpolateTo(self.getPosition(), pos, dt, speed)
-        obj.setPosition(newPos)
-
-        if (self.getPosition().distance(pos) < 1) 
-        {
-            moving = false
-            self.onTick.remove(smoothOnTick)
-            obj.setObjectType(oldType)
-
-            if (callback) callback(obj)
-        }
-    }
-
-    obj.setObjectType(2) // set penetrable
-    obj.onTick.add(smoothOnTick)
-
-    setTimeout(() => {
-        // If it's still moving after 5 seconds, stop.
-        if (moving && obj) 
-        {
-            obj.onTick.remove(smoothOnTick)
-            obj.setObjectType(oldType)
-            if (callback) callback(obj)
-        }
-    }, 5000);
-}
-
 module.exports.format = format;
 module.exports.isValidGameObject = isValidGameObject;
 module.exports.isPlayer = isPlayer;
 module.exports.isGameObject = isGameObject;
-module.exports.isServerHost = isServerHost;
-module.exports.setPositionSmooth = setPositionSmooth;
